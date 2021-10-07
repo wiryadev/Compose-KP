696edd27650e5eef7560a9b897c446febe33c6df
wiryadev <abrarwiryawan@gmail.com>
2021-10-07 08:46:15 +0700

    Initial commit

---
+++ app/src/main/java/com/wiryadev/composekp/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.wiryadev.composekp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.wiryadev.composekp.ui.theme.ComposeKPTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeKPTheme {
+                // A surface container using the 'background' color from the theme
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    ClickableComposable()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Greeting() {
+    Text(
+        text = "Hello World!",
+        color = Color.Blue,
+        fontWeight = FontWeight.Bold,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowImage() {
+    Image(
+        painter = painterResource(
+            id = R.drawable.cat
+        ),
+        contentDescription = "Cat"
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ColumnExample() {
+    Column {
+        Greeting()
+        Greeting()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RowExample() {
+    Row {
+        Greeting()
+        Greeting()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BoxPreview() {
+    Box {
+        ShowImage()
+        Text(
+            text = "Menimpa Gambar",
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ClickableComposable() {
+    val counter = remember {
+        mutableStateOf(0)
+    }
+
+    Button(
+        onClick = { counter.value++ }
+    ) {
+        Text(text = "Diklik: ${counter.value} kali")
+    }
+}
